9993ac8f27978c1726e84777e54f0d4a43653d46
AbhinavDevelops <abhinav.rajaram02@gmail.com>
2024-07-17 15:28:36 +0800

    added techstack and hyperlinks

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,16 +8,21 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 const Navbar = () => {
     return (<nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img className="mx-2 w-10"
-                src={logo}
-                alt="logo"
-            >
-            </img>
+            <a href="/">
+                <img className="mx-2 w-10"
+                    src={logo}
+                    alt="logo"
+                >
+                </img>
+            </a>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl text-white">
-            <FaLinkedin />
-            <FaGithub />
-            <FaSquareXTwitter />
+            <a href="https://www.linkedin.com/in/abhinav-rajaram/" target="_blank">
+                <FaLinkedin />
+            </a>
+            <a href="https://github.com/AbhinavDevelops" target="_blank">
+                <FaGithub />
+            </a>
         </div>
 
     </nav>);
--- src/components/technologies.jsx
+++ src/components/technologies.jsx
@@ -1,18 +1,53 @@
 import { RiReactjsLine } from 'react-icons/ri';
 import hf_logo from '../assets/hf-logo.png';
+import python_logo from "../assets/python.png"
+import llama_logo from "../assets/llama.png"
+import { BiLogoPostgresql } from 'react-icons/bi';
 
 const Technologies = () => {
     return (
         <div className="border-b border-neutral-800 p-24">
             <h1 className="my-17 text-center text-4xl">Technologies</h1>
-            <div className="flex flex-wrap items-center justify-center gap-4">
-                <div className="rounded-2xl border-4 border-purple-900 p-4">
-                    <RiReactjsLine className="text-7xl text-blue-500" />
+            <div className="flex flex-wrap items-center justify-center gap-4 p-8">
+
+
+                <div className="rounded-2xl border-4 border-purple-800 p-4 ">
+                    <a href="https://www.python.org/" target="_blank">
+                        <img src={python_logo} alt="Python logo" className="w-20 text-7xl" />
+                        <p className="text-xs text-center">Python</p>
+                    </a>
+
+                </div>
+
+                <div className="rounded-2xl border-4 border-purple-800 p-4 ">
+                    <a href="https://huggingface.co/" target="_blank">
+                        <img src={hf_logo} alt="HF Logo" className="w-20 text-7xl" />
+                        <p className="text-xs text-center">Hugging Face</p>
+                    </a>
+
+                </div>
+
+
+                <div className="rounded-2xl border-4 border-purple-800 p-4">
+                    <a href="https://react.dev" target="_blank">
+                        <RiReactjsLine className="text-7xl text-blue-500" />
+                    </a>
                     <p className="text-lg text-center">React</p>
                 </div>
+
                 <div className="rounded-2xl border-4 border-purple-800 p-4 ">
-                    <img src={hf_logo} alt="HF Logo" className="w-20" />
-                    <p className="text-xs text-center">Hugging Face</p>
+                    <a href="https://llama.meta.com/llama3/" target="_blank">
+                        <img src={llama_logo} alt="Llama logo" className="w-20 text-3xl" />
+                        <p className="text-xs text-center">Llama3</p>
+                    </a>
+
+                </div>
+
+                <div className="rounded-2xl border-4 border-purple-800 p-4 ">
+                    <a href="https://llama.meta.com/llama3/" target="_blank">
+                        <BiLogoPostgresql className="text-7xl text-blue-500" />
+                        <p className="text-xs text-center">Llama3</p>
+                    </a>
 
                 </div>
 
